0af29d31600fe2a0665ea3ed2c7b49bfd15f9abc
Nihanth2709 <27nihanth02009@gmail.com>
2026-04-02 19:23:47 +0530

    leapyear

---
+++ leapyear.py
@@ -0,0 +1,9 @@
+year = int(input("enter the year: "))
+if year % 4 == 0:
+    if year % 100 != 0:
+        print("it is leap year")
+    else:
+         print("it is not leap year")
+else:
+     print("it is not leap year")
+      
