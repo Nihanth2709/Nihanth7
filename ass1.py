177611a1d9ce0bc3f897b1675dcbd20c0cdca8a3
Nihanth2709 <27nihanth02009@gmail.com>
2026-04-03 19:42:44 +0530

    python assignment1

--- ass1.py
+++ ass1.py
@@ -20,7 +20,70 @@ maths = int(input("enter the maths marks: "))
 science = int(input("enter the science marks: "))
 english = int(input("enter the english marks: "))
 total = maths + science + english
-if (total = 40):
+if (total <= 40):
     print("excellent, you have passed")
-elif (total < 40):
-    
+elif (total > 33):
+    print("excellent, you have passed")
+else:
+    print("you have failed")
+
+#A spam comment is defined as a text containing following keywords: “Make a lot of money”, “buy now”, “subscribe this”, “click this”. Write a program to detect these spams. 
+list = ["Make a lot of money”, “buy now”, “subscribe this”, “click this"]
+comment = input("enter the comment: ")
+if list in comment:
+   print("it is a spam comment")
+else:
+    print("it is not a spam comment")
+
+#Write a program to find whether a given username contains less than 10 characters or not. 
+username = input("enter the name: ")
+if len(username) < 10:
+    print(username,"contains more than 10 characters")
+else:
+     print(username,"contains less than 10 characters")
+
+# Write a program which finds out whether a given name is present in a list or not. 
+fruits = ["apple","mango","banana","grapes"]
+fruit_name = input("enter the fruit name: ")
+if fruit_name in fruits:
+    print(fruit_name,"is present")
+else:
+    print(fruit_name,"is not present")
+
+#Write a program to calculate the grade of a student from his marks from the 
+#following scheme: 
+#90 – 100 => Ex 
+#80 – 90 => A 
+#70 – 80 => B 
+#60 – 70  =>C 
+#50 – 60 => D 
+#<50 => F
+
+grade = input("enter the grade of student: ")
+if grade > 90:
+    print(" grade:excellent")
+elif grade > 80:
+    print(" grade:A")
+elif grade > 70:
+    print(" grade:B")
+elif grade > 60:
+    print(" grade:C")
+if grade >= 50:
+    print(" grade:D")
+else:
+    print("grade:fail(F)")
+
+#Write a program to find the greatest of four numbers entered by the user. 
+num1 = int(input("enter the first number: "))
+num2 = int(input("enter the second number: "))
+num3 = int(input("enter the third number: "))
+num4 = int(input("enter the fourth number: "))
+greater = num1
+if num1 < num2:
+    greater = num2
+if greater < num3:
+    greater = num3
+if greater < num4:
+    greater = num4
+print(greater,"is greater than all")
+
